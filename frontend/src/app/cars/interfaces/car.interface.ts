d628adb8942ac3af8a99e1e4ba80e5fff506f344
Samuel Luna Ojeda <slunaoje@emeal.nttdata.com>
2025-07-29 09:41:47 +0200

    Crear interfaz para CarDetails. Modificar nombre de la anterior interfaz

--- frontend/src/app/cars/interfaces/car.interface.ts
+++ frontend/src/app/cars/interfaces/car.interface.ts
@@ -3,4 +3,15 @@ export interface CarResponse {
     model: string
     id: string
     total: number
+    carDetails: CarDetails[]
+}
+
+export interface CarDetails {
+    availability: boolean
+    currency: string
+    licensePlate: string
+    manufactureYear: number
+    mileage: number
+    price: number
+    registrationDate: Date
 }
---
+++ frontend/src/app/cars/interfaces/cars.interface.ts
@@ -0,0 +1,6 @@
+export interface CarsResponse {
+    brand: string
+    model: string
+    id: string
+    total: number
+}
