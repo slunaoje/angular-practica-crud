036ac0759ffe3860a66ca91d8ffa06ab7d6a1e54
Samuel Luna Ojeda <slunaoje@emeal.nttdata.com>
2025-07-29 09:47:15 +0200

    Cambios en TableComponent en los estilos y añadir opción Ver en el menú contextual y llama a CarDetailsComponent

--- frontend/src/app/Pages/table/table.component.ts
+++ frontend/src/app/Pages/table/table.component.ts
@@ -8,6 +8,7 @@ import {
 } from '@angular/cdk/menu'
 import { Component, inject } from '@angular/core'
 import { rxResource } from '@angular/core/rxjs-interop'
+import { Router } from '@angular/router'
 import { BasicButtonDirective } from '../../basic-button.directive'
 import { CarsService } from '../../cars/services/cars.service'
 
@@ -27,20 +28,24 @@ import { CarsService } from '../../cars/services/cars.service'
 })
 export class TableComponent {
     carsService = inject(CarsService)
+    router = inject(Router)
     colorPrimary = 'rgb(33 158 188)'
     colorSecondary = 'rgb(255, 183, 3)'
-    bold = false
-    italic = false
 
     carsResource = rxResource({
         loader: () => this.carsService.getCars(),
     })
-    sizes = ['Small', 'Normal', 'Large']
-    selectedSize: string | undefined = 'Normal'
 
-    reset() {
-        this.bold = false
-        this.italic = false
-        this.selectedSize = 'Normal'
+    see(idCar: string) {
+        console.log(idCar)
+        this.router.navigate([`/cars/${idCar}`])
+    }
+
+    edit() {
+        console.log('edit')
+    }
+
+    remove() {
+        console.log('remove')
     }
 }
